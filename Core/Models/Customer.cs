a05d325ca1950d21d88caee56af4c8ebdbd9a44f
dogukanergezer <52700986+dogukanergezer@users.noreply.github.com>
2020-09-19 13:38:32 +0300

    Add project files.

---
+++ Core/Models/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Address { get; set; }
+        public char[] IdentityNumber { get; set; }
+        public string Country { get; set; }
+        public char[] Phone { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
